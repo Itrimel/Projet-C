1d9a94b08b9ea13ea6dacc4b5bdc55c04d460d76
Théo Caroff <tcaroff@mac8.info.espci.fr>
2017-01-18 09:10:25 +0100

    envoi fichier de base

---
+++ graphics.cc
@@ -0,0 +1,52 @@
+//https:www.cairographics.org/manual/cairo-Paths.html
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <time.h>
+#include <assert.h>
+#include "Graphics.h"
+
+void
+Graphics::draw(Particle *p){
+  
+  const int FPS=50;//frames per second
+  struct timespec tm={0,1000000000/FPS}; //Sleep in nanoseconds between frames
+  nanosleep( &tm , NULL);
+  XEvent event;//check if window closed and finish
+  if( XCheckWindowEvent(dsp,da, DestroyNotify , &event)){ XCloseDisplay(dsp); exit(1);}
+
+  double gamma = Dim/(lmax-lmin + diam);//scaling between physical units and pixels
+  double alpha= gamma*diam/2.;
+  
+  cairo_push_group(cr); //start drawing
+  cairo_set_source_rgb(cr, 0.0, 0.19, 0);//dark green background
+  cairo_paint (cr); //clear screen with green
+
+  cairo_set_source_rgb(cr, 0.69, 0.19, 0);//dark red for particles
+  for(int i=0;i<Np;i++){// place the particles in the graphics buffer, without drawing
+    cairo_new_sub_path(cr) ;
+    cairo_arc(cr,  alpha + gamma* (p[i].x -lmin) ,  alpha + gamma*(p[i].y - lmin), alpha, 0, 2 * M_PI);
+  }
+  cairo_fill(cr);//draw all particles with solid color
+
+  /*  cairo_set_source_rgb(cr, 0, 0.19, 0.69);//dark blue for particles
+  for(int i=0;i<Np;i++){
+    cairo_new_sub_path(cr) ;
+    cairo_arc(cr,  alpha + gamma* (p[i].x -lmin) ,  alpha + gamma*(p[i].y - lmin), 1.4*alpha, 0, 2 * M_PI);
+  }
+  cairo_stroke (cr); // hollow particles*/
+
+  
+  frame(alpha, Dim-alpha, alpha, Dim-alpha);//draw square border
+  cairo_pop_group_to_source(cr); //finished drawing operations for this set of positions
+  cairo_paint(cr);//send to screen
+  
+}
+
+void Graphics::frame(double xmin, double xmax, double ymin, double ymax){
+  cairo_set_source_rgb (cr, 1, 1, 0);//yellow border
+  cairo_rectangle (cr,xmin, ymin, xmax-xmin, ymax-ymin);
+  cairo_stroke (cr);
+}
---
+++ start.cc
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include "Graphics.h"  // Contains the definition of "Particule" and "Graphics"
+
+enum  col_type
+{
+ 	bottom,
+ 	right,
+	top,
+  	left,
+	animation,
+	particle
+}; //different types of collision
+
+typedef struct
+{ // a structure describing each collision -- one might want an array of Events
+	enum col_type type;
+	int ia,ib;
+ 	double time;
+} Event;
+
+void initparticles( Particle *p, int np, double Lmin, double Lmax, double diameter)
+{
+	int i;
+	for( i=0;i<np;i++)
+	{ // drand48() -- random double numbers uniform on (0 1)
+		p[i].x = Lmin +diameter/2 + (Lmax-Lmin-diameter)*drand48(); //random positions for intial condition
+		p[i].y = Lmin +diameter/2 + (Lmax-Lmin-diameter)*drand48();
+		p[i].vx = 0;// choose random speeds too using drand48();
+		p[i].vy = 0;
+  	}
+}
+
+int main()
+{
+	int Np=2; //Number of particles
+	double diameter=1;//particle size
+	int Pix=800; //Number of pixels for window
+	double Lmax=10, Lmin=0; //Physical dimensions of box
+	Graphics gw(Np,Pix, Lmin ,Lmax,diameter);// Open a window to plot particles in
+	srand48(1);//inititalize random numbers -- to find always the same value // you can replace "1" by time(NULL) 
+  
+	Particle *p= (Particle *) malloc( Np *sizeof( Particle)); //an array of particles
+	initparticles(p,Np,Lmin, Lmax,diameter); //place particles in box
+	Event *e = (Event *) malloc( 4*Np* sizeof(Event) ); // 4 Np possible collisions with walls
+  
+	for (int l=0; l<10000;l++)
+	{//long loop
+	//find future collision using exact calculations of collision times
+	// e[0].type=right;
+	// e[0].time = ...
+	// find the very first collision in the future by looking at e[].time
+	// move particles
+	gw.draw(p);
+  }
+	free(p);
+	free(e);
+  return 0;
+}
+
+
+
+
